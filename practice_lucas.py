ff1a87aa0855dd467f0f981c0520231f86b022df
Lucas_Kim <kkamcci20@hotmail.com>
2023-07-25 15:31:25 -0700

    2nd commit

--- practice_lucas.py
+++ practice_lucas.py
@@ -7,6 +7,36 @@ print("Leader is in charge")
 
 
 
+랜덤함수
+from random import * # random library에 있는 함수를 사용
+
+print(random()) #0.0이상 1.0 미만의 임의의 값을 생성
+print(random()*10) #0.0이상 10.0 미만의 임의의 값을 생성
+print(int(random() * 10)) # 0.0이상 10.0 미만의 임의의 정수값을 생성
+print(int(random() * 10)) # 0.0이상 10.0 미만의 임의의 정수값을 생성
+print(int(random() * 10)) # 0.0이상 10.0 미만의 임의의 정수값을 생성
+print(int(random() * 10)+1) # 1.0이상 10.0 이하의 임의의 정수값을 생성
+print(int(random() * 10)+1) # 1.0이상 10.0 이하의 임의의 정수값을 생성
+print(int(random() * 10)+1) # 1.0이상 10.0 이하의 임의의 정수값을 생성
+print(int(random() * 10)+1) # 1.0이상 10.0 이하의 임의의 정수값을 생성
+print(int(random() * 10)+1) # 1.0이상 10.0 이하의 임의의 정수값을 생성
+print(int(random() * 10)+1) # 1.0이상 10.0 이하의 임의의 정수값을 생성
+
+print(int(random() * 65)+1) # 1.0이상 65.0 이하의 임의의 정수값을 생성(로또)
+print(int(random() * 65)+1) # 1.0이상 65.0 이하의 임의의 정수값을 생성(로또)
+print(int(random() * 65)+1) # 1.0이상 65.0 이하의 임의의 정수값을 생성(로또)
+print(int(random() * 65)+1) # 1.0이상 65.0 이하의 임의의 정수값을 생성(로또)
+print(int(random() * 65)+1) # 1.0이상 65.0 이하의 임의의 정수값을 생성(로또)
+print(int(random() * 65)+1) # 1.0이상 65.0 이하의 임의의 정수값을 생성(로또)
+print(int(random() * 65)+1) # 1.0이상 65.0 이하의 임의의 정수값을 생성(로또)
+
+print(randrange(1,66)) # 1~66미만의 임의의 값 생성
+print(randrange(1,66)) # 1~66미만의 임의의 값 생성
+print(randrange(1,66)) # 1~66미만의 임의의 값 생성
+print(randrange(1,66)) # 1~66미만의 임의의 값 생성
+print(randrange(1,66)) # 1~66미만의 임의의 값 생성
+print(randrange(1,66)) # 1~66미만의 임의의 값 생성
+
 상속
 일반유닛
 class Unit:
